90574d69f2203f168598cbf700a83e76cb85d968
Collins Lagat <cklagat@gmail.com>
2021-12-15 09:28:14 +0300

    feat: simple implementation

--- app/assets/typescript/main.ts
+++ app/assets/typescript/main.ts
@@ -1,1 +1,14 @@
 import '../scss/main.scss'
+import { Application, Controller } from '@hotwired/stimulus'
+
+declare const Stimulus: Application
+
+class stepController extends Controller {
+  public done = false
+
+  connect() {
+    console.log(this.done)
+  }
+}
+
+Stimulus.register('step', stepController)
