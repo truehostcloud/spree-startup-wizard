20384e1fdf6671a0d54186bc01347ede00660809
Collins Lagat <cklagat@gmail.com>
2021-12-21 15:17:57 +0300

    feat: unique index

--- db/migrate/20211215061338_create_startup_wizard_statuses.rb
+++ db/migrate/20211215061338_create_startup_wizard_statuses.rb
@@ -5,6 +5,7 @@ class CreateStartupWizardStatuses < ActiveRecord::Migration[6.1]
       t.bigint :store_id, null: false
       t.boolean :done, null: false, default: false
 
+      t.index %w[checklist_id store_id], name: 'index_startup_wizard_checklists_on_checklist_id_and_store_id', unique: true
       t.index :checklist_id, name: 'index_startup_wizard_checklists_on_checklist_id'
       t.index :store_id, name: 'index_startup_wizard_statuses_on_store_id'
 
